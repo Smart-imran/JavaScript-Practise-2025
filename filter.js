f3f3179242ace4b6bf83f17a87957f3bab9361e6
Smart-imran <rst.imran4@gmail.com>
2025-03-26 02:16:21 +0600

    filter, map, find

---
+++ filter.js
@@ -0,0 +1,16 @@
+/* const players = [75,77,80,92,95,75,63,100];
+
+const selected = players.filter(p=> p>70);
+
+console.log(selected); */
+
+// string filter
+
+/* const fruits = ["Apple", "Banana", "orange", "watermelon"];
+
+//const selectedFood = fruits.filter(fruit => fruit[0].toLocaleLowerCase()=== 'o');
+
+const length1 = fruits.filter(f => (f.length > 5) && (f.toLowerCase().includes('o')));
+
+console.log(length1); */
+
