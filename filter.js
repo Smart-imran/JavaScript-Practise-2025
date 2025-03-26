36a5e67cb2c0aa325ed11a4c5b026d9bb24f589e
Smart-imran <rst.imran4@gmail.com>
2025-03-27 01:02:19 +0600

    problem solve

--- filter.js
+++ filter.js
@@ -14,3 +14,29 @@ const length1 = fruits.filter(f => (f.length > 5) && (f.toLowerCase().includes('
 
 console.log(length1); */
 
+// problem : 1,  [30,45,20,60,10] i want add 13 using map
+
+/* const items = [30,45,20,60,10];
+
+const addItems = items.map(item => item + 13);
+
+console.log(addItems); */
+
+
+// problem : 2 ["Messi","Maradona", "play", "ronaldo", "Zidane"]
+
+
+/* const players =  ["Messi","Maradona", "play", "ronaldo", "Zidane"];
+
+const filterring =  players.filter( player => (player.length > 5) && (player.toLowerCase().includes("on")) );
+
+console.log(filterring); */
+
+
+//problem -3 [10,15,20,25,30,35]
+
+/* const numbers = [10,15,20,25,30,35];
+
+const findLarge20 =  numbers.find(number => number > 20);
+
+console.log(findLarge20); */
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,5 @@
-console.log(`${num} Imran `);
+const players =  ["Messi","Maradona", "play", "ronaldo", "Zidane"];
+
+const filterring =  players.filter( player => (player.length > 5) && (player.toLowerCase().includes("on")) );
+
+console.log(filterring);
