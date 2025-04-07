c45383797099f2062b00771087d2df0ac271be28
Smart-imran <rst.imran4@gmail.com>
2025-04-07 23:29:41 +0600

    promise

--- JSON.js
+++ JSON.js
@@ -15,7 +15,7 @@ const user2Object = JSON.parse(user2);
 
 console.log(user2Object); */ // { id: 1, name: 'John Doe', job: 'Software Engineer', age: 30 }
 
-const getUsers = new Promise((resolve, reject) => {
+/* const getUsers = new Promise((resolve, reject) => {
 
     const usersAvailable = true;
 
@@ -41,6 +41,40 @@ getUsers
     })
     .catch((error) => {
         console.log(error)
+    }); */
+
+
+/* const  getUsers =  new Promise ((resolve, reject)=>{
+
+    const availableUsers = true;
+    const users = [
+        { id: 1, name: "John Doe", job: "Software Engineer", age: 30 },
+        { id: 2, name: "Jane Smith", job: "Data Scientist", age: 28 },
+        { id: 3, name: "Alice Johnson", job: "Web Developer", age: 25 }
+    ];
+
+}) */
+
+// Problem 1: Promise to boil water and make tea
+
+function boilWater() {
+
+    return new Promise((resolve,reject) => {
+        let boil = true;
+        if (boil) {
+            resolve("Water is boiled");
+        }
+        else {
+            reject("Water is not boiled")
+        }
+
     });
+}
 
-
+boilWater()
+    .then((message) => {
+        console.log(message);
+    })
+    .catch((error) => {
+        console.log(error);
+    })
